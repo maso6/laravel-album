76d140b326be9b9a20f90cdb189c30244183b83d
Mads Lind <mads@madslind.nu>
2025-03-13 09:51:28 +0100

    css changes

--- resources/views/public/gallery.blade.php
+++ resources/views/public/gallery.blade.php
@@ -7,15 +7,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
-        img.card-img-top {
+      img.card-img-top {
         background-color: #000;
-        }
-        p.card-text {
-        min-height: 100px;
-    }
-    body {
+      }
+      body {
         background-color: #f8f9fa!important;
-    }
+      }
     </style>
     <title>{{ $gallery->title }}</title>
   </head>
